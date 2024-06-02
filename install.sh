fcc7089b20501bdbf654c0abb14665492a2c7e20
MrKeith99 <kvalentincardenas@hotmail.com>
2024-06-02 21:27:18 +0900

    [UPD] add ccf repo

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ sudo apt-get install -y \
     ros-$ROS_DISTRO-usb-cam \
     ros-$ROS_DISTRO-image-view
 
+# Clone ccf_person_identification repository
+cd ../
+git clone https://github.com/TeamSOBITS/ccf_person_identification.git
 
 # Return to the original directory
 cd $DIR
