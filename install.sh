f01ff1b58f7f5d966cfeb32a2c81c62f7f7fe8cb
MrKeith99 <kvalentincardenas@hotmail.com>
2024-06-05 16:40:59 +0900

    [UPD] new dependencies

--- install.sh
+++ install.sh
@@ -8,11 +8,18 @@ DIR=`pwd`
 
 # Download dependencies
 sudo apt-get install -y \
+    ros-$ROS_DISTRO-cv-bridge \
+    ros-$ROS_DISTRO-tf \
+    ros-$ROS_DISTRO-tf-conversions \
+    ros-$ROS_DISTRO-image-transport \
+    ros-$ROS_DISTRO-message-filters \
+    ros-$ROS_DISTRO-sensor-msgs \
+    ros-$ROS_DISTRO-visualization-msgs \
     ros-$ROS_DISTRO-usb-cam \
     ros-$ROS_DISTRO-image-view
 
 # Clone required repositories
-cd ../
+cd $DIR/../
 git clone https://github.com/TeamSOBITS/ccf_person_identification.git
 cd ccf_person_identification/
 bash install.sh
@@ -21,7 +28,6 @@ cd $DIR/../
 git clone https://github.com/TeamSOBITS/lightweight_human_pose_estimation.git
 cd lightweight_human_pose_estimation/
 bash install.sh
-
 
 # Return to the original directory
 cd $DIR
